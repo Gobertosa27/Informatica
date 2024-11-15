f0d0fabef2d15e1dfa2d7da0de128eacfcb601b7
Gobertosa27 <114988679+Gobertosa27@users.noreply.github.com>
2024-11-15 13:37:37 +0100

    Update file.cpp

--- file.cpp
+++ file.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <cmath>
-#include <fstream>
+#include <fstream>//ricordati di questo!
 using namespace std;
+
+int minimo(int v[], int);
+int massimo(int v[], int);
 
 int main ()
 {
@@ -17,7 +20,8 @@ int main ()
 
     for(int k=0; k<10; k=k+1)
     {
-        flusso_in>>dati[k];
+        flusso_in>>dati[k];//sarebbe il cin dei fle. I valori, al posto di essere
+        //assegnati da tastiera, vengono assegnati da quelli dei file
     }
     
     flusso_in.close();
@@ -35,9 +39,8 @@ int main ()
     
     while(!flusso_in.eof())
     {
-      conta=conta+1;
-
-      flusso_in >> dato;
+        flusso_in >> dato;
+        conta=conta+1;
     }
 
     cout<<endl<<"Sono presenti "<<conta<<" dati interi"<<endl;
@@ -54,5 +57,64 @@ int main ()
     cout<<endl;
     flusso_in.close();
 
+    //ricerca min
+    int min;
+    min=minimo(dati, dati_used);
+    cout<<"Il min e': "<<min<<endl;
+    int count=0;
+    for(int k=0; k<dati_used; k=k+1)
+    {
+       if(dati[k]==min) 
+       {
+        if(count==0) 
+        {
+            cout<<"In posizione "<<k;
+            count=count+1;
+        }
+        else cout<<", "<<k;  
+       }
+    }
+
+    cout<<endl;
+    //ricerca max
+    int max;
+    max=massimo(dati, dati_used);
+    cout<<"Il max e': "<<max<<endl;
+    count=0;
+    for(int k=0; k<dati_used; k=k+1)
+    {
+       if(dati[k]==max) 
+       {
+        if(count==0) 
+        {
+            cout<<"In posizione "<<k;
+            count=count+1;
+        }
+        else cout<<", "<<k;  
+       }
+    }
+    cout<<endl;
     return 0;
+}
+
+int minimo(int v[], int dim)
+{
+    int min;
+    min=v[0];
+    for(int k=0; k<dim; k=k+1)
+    {
+       if(v[k]<min) min=v[k]; 
+    }
+    return min;
+}
+
+int massimo(int v[], int dim)
+{
+    int max;
+    max=v[0];
+    for(int k=0; k<dim; k=k+1)
+    {
+       if(v[k]>max) max=v[k]; 
+    }
+    return max;
 }
