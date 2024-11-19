fcb5c9c0da64b4b5012c156c6315637f11da48f4
Gobertosa27 <114988679+Gobertosa27@users.noreply.github.com>
2024-11-19 23:50:38 +0100

    Update scambio_arraydin.cpp

--- scambio_arraydin.cpp
+++ scambio_arraydin.cpp
@@ -6,25 +6,29 @@ using namespace std;
 
 void scambiafloat(float *, float *);
 void scambiaint(int *, int *);
+void ordinamentoint(int *, int*);
 
 int main()
 {
-   int *p1, *p2;
+   int *p1, *p2;//puntatore = contenuto dell'indirizzo a cui il puntatore punta
    
    int n;
    cout<<"Inserire numero di el. da inserire: ";
    cin>>n;
 
    cout<<"Inserire vettori di "<<n<<" el. interi:"<<endl;
-   p1=new int[n]; //n*sizeof(float)
+   p1=new int[n]; /*n*sizeof(float) restituisce l'indirizzo a cui è allocato dinamicamente l'array e, poi,
+   p1 ne legge il valore nella cella*/
    p2=new int[n];
     int i=0;
     while(i<=n)
     {
         cout<<"p1["<<i<<"]=";
+        cout<<"("<<&p1[i]<<")";
         cin>>p1[i];
-
+       
         cout<<"p2["<<i<<"]=";
+        cout<<"("<<&p2[i]<<")";
         cin>>p2[i];
         
         i=i+1;
@@ -43,6 +47,12 @@ int main()
 
         i=i+1;
     }
+
+
+
+    delete [] p1, p2;
+    p1=NULL;
+    p2=NULL;
     return 0;
 }
 
@@ -52,4 +62,9 @@ void scambiaint(int *p1, int *p2)
     appo=*p1;
     *p1=*p2;
     *p2=appo;
+}
+
+void ordinamento (int *p1, int *p2)
+{
+    
 }
