4812f1bd3ffbf8a95b0cd44876149cc1eedc3c74
Gobertosa27 <114988679+Gobertosa27@users.noreply.github.com>
2024-12-09 23:31:31 +0100

    Update Merge.cpp

--- Merge.cpp
+++ Merge.cpp
@@ -18,12 +18,28 @@ int main ()
     cout<<"Inserisci "<<dim<<" elementi:"<<endl;
     for(int k=0; k<dim; k++)
     {
-        cout<<"v["<<k<<"]="<<v[k]<<endl;
+        cout<<"v["<<k<<"]=";
+        cin>>v[k];
     }
-    centro=dim/2;
-
+    centro=dim;
     cout<<"Algoritmo merge:"<<endl;
-    Merge(v, )
+    Merge(v, centro, dim);
 
     return 0;
+}
+
+void Merge(float *v, int centro, int dim)
+{
+    dim=centro;
+    if(dim>1)
+    {
+        if(dim%2==0)  centro=dim/2;
+        else centro=(dim+1)/2;
+        for(int k=0; k<centro; k++)
+        {
+              cout<<"v["<<k<<"]="<<v[k]<<", ";
+        }
+        cout<<endl;
+        Merge(v, centro, dim);
+    }
 }
